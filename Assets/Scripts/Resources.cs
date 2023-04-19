6ef90708bd83f538b638e113a8133eccbb9e7334
Dmitry Guznenok <108734067+Medsia@users.noreply.github.com>
2023-04-19 22:47:32 +0300

    Add death panel and edit game speed button

--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameResources : MonoBehaviour
@@ -8,9 +10,12 @@ public class GameResources : MonoBehaviour
     public int gold, income, lives;
     public Text goldText;
     public Text livesText;
+    public GameObject deathPanel;
+    public Button speedBtn;
 
     public void Start()
     {
+        deathPanel.SetActive(false);
         goldText.text = gold.ToString();
         livesText.text = lives.ToString();
     }
@@ -30,5 +35,31 @@ public class GameResources : MonoBehaviour
     {
         lives--;
         livesText.text = lives.ToString();
+        if (lives <= 0)
+        {
+            deathPanel.SetActive(true);
+        }
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ChangeGameSpeed()
+    {
+        if (Time.timeScale == 0.5)
+        {
+            Time.timeScale = 1;
+        }
+        else if (Time.timeScale == 1)
+        {
+            Time.timeScale = 2;
+        }
+        else if (Time.timeScale == 2)
+        {
+            Time.timeScale = 0.5f;
+        }
+        speedBtn.GetComponentInChildren<TextMeshProUGUI>().text = Time.timeScale + "x";
     }
 }
